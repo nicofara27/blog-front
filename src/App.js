69cbf7ceafcec2f4d445e6db46c339ade1954620
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-07-10 19:30:59 -0300

    Ruta a pagina principal agregada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
-import './App.css';
-import Menu from './components/common/Menu';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Menu from "./components/common/Menu";
+import PagPrincipal from "./components/views/PagPrincipal";
 
 function App() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        
+        <Route exact path="/" element={<PagPrincipal></PagPrincipal>}></Route>
       </Routes>
     </BrowserRouter>
   );
