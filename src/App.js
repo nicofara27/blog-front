a94bfb6b04b8e93f133654f341ac3926351effe2
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-07-11 13:16:08 -0300

    Ruta para la pagina de cada articulo agregada

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Menu from "./components/common/Menu";
 import PagPrincipal from "./components/views/PagPrincipal";
+import PagArticulo from "./components/views/PagArticulo";
 
 function App() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route exact path="/" element={<PagPrincipal></PagPrincipal>}></Route>
+        <Route exact path="/" element={<PagPrincipal/>}></Route>
+        <Route exact path="/articulo/:id" element={<PagArticulo/>}></Route>
       </Routes>
     </BrowserRouter>
   );
