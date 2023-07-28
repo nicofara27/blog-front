b78c38e1be1fd34ddd9d5e359e5b5d05a760ca1c
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-07-28 19:08:08 -0300

    Corregi la ruta del boton para ver mas sobre un articulo

--- src/components/views/pagPrincipal/Articulo.jsx
+++ src/components/views/pagPrincipal/Articulo.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
 
 const Articulo = ({articulo}) => {
-    console.log(articulo)
     return (
         <Row className='my-5 articulo'>
             <Col className='d-flex flex-column justify-content-between'>
             <h1 className='fw-bolder'>{articulo.titulo}</h1>
             <p>{articulo.texto}</p>
-            <Button href='/articulo/asd' variant="outline-primary">Ver mas</Button>
+            <Button href={`/articulo/${articulo.id}`} variant="outline-primary">Ver mas</Button>
             </Col>
             <Col>
                 <div className='articulo__imgContainer'>
