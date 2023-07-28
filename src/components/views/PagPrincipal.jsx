54df8ef3ccd01e4b3739f1dc9dff09b259dea2e0
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-07-28 19:07:18 -0300

    Cambie la funcion para traer la lista de articulos para que ahora filtre por categoria

--- src/components/views/PagPrincipal.jsx
+++ src/components/views/PagPrincipal.jsx
@@ -2,27 +2,30 @@ import React, { useEffect, useState } from "react";
 import Articulo from "./pagPrincipal/Articulo";
 import { Container } from "react-bootstrap";
 import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 const PagPrincipal = () => {
   const [articulos, setArticulos] = useState([]);
 
+  const categoria = useLocation().search;
+
   useEffect(() => {
     const traerArticulos = async () => {
       try {
-        const respuesta = await axios.get("http://localhost:4000/articulos");
+        const respuesta = await axios.get(`http://localhost:4000/articulos${categoria}`);
         setArticulos(respuesta.data);
       } catch (err) {
         console.log(err);
       }
     };
     traerArticulos();
-  }, []);
+  }, [categoria]);
 
   return (
     <main>
       <Container>
         {articulos.map((articulo) => (
-          <Articulo articulo={articulo}></Articulo>
+          <Articulo key={articulo.id} articulo={articulo}></Articulo>
         ))}
       </Container>
     </main>
