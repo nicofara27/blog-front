b6ee66750cc9674d9183643fd79b5de78acb51f3
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-07-30 12:08:14 -0300

    Agregue funcion para traer un articulo

--- src/components/views/PagArticulo.jsx
+++ src/components/views/PagArticulo.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import ArticuloInd from './pagArticulo/ArticuloInd';
-import ArticulosRec from './pagArticulo/ArticulosRec';
-import { Container } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import ArticuloInd from "./pagArticulo/ArticuloIndiv";
+import ArticulosRec from "./pagArticulo/ArticulosRec";
+import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const PagArticulo = () => {
-    return (
-        <main>
-            <Container className='d-flex justify-content-between'>
-            <ArticuloInd/>
-            <ArticulosRec/>
-            </Container>
-        </main>
-    );
+  const [articulo, setArticulo] = useState({});
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const traerArticulos = async () => {
+      try {
+        const respuesta = await axios.get(
+          `http://localhost:4000/articulos/${id}`
+        );
+        setArticulo(respuesta.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    traerArticulos();
+  }, []);
+
+  return (
+    <main>
+      <Container className="d-flex justify-content-between">
+        <ArticuloInd articulo={articulo}/>
+        <ArticulosRec />
+      </Container>
+    </main>
+  );
 };
 
 export default PagArticulo;
