8772bfd75f2787907fac512195438962ea323efa
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-01 18:10:17 -0300

    Funcion para traer articulos recomendados agregada

--- src/components/views/pagArticulo/ArticuloRecIndiv.jsx
+++ src/components/views/pagArticulo/ArticuloRecIndiv.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button } from "react-bootstrap";
 
-const ArticuloRecInd = () => {
-    return (
-        <article className='mb-4'>
-            <div>
-                <img src="https://kinsta.com/es/wp-content/uploads/sites/8/2020/10/tipos-de-archivos-de-imagen.png" alt="" className='w-100'/>
-            </div>
-            <h6 className='fw-bolder'>Titulo prueba</h6>  
-            <Button className='fs-7 py-0 px-1 w-100' variant='outline-primary'>Ver mas</Button>
-        </article>
-    );
+const ArticuloRecInd = ({ articulo }) => {
+  return (
+    <article className="mb-4">
+      <div>
+        <img src={articulo.img} alt={articulo.titulo} className="w-100" />
+      </div>
+      <h6 className="fw-bolder">{articulo.titulo}</h6>
+      <Button
+        className="fs-7 py-0 px-1 w-100"
+        variant="outline-primary"
+        href={`/articulo/${articulo.id}`}
+      >
+        Ver mas
+      </Button>
+    </article>
+  );
 };
 
 export default ArticuloRecInd;
--- src/components/views/pagArticulo/ArticulosRec.jsx
+++ src/components/views/pagArticulo/ArticulosRec.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import { Col } from 'react-bootstrap';
-import ArticuloRecInd from './ArticuloRecIndiv';
+import React, { useEffect, useState } from "react";
+import { Col } from "react-bootstrap";
+import ArticuloRecInd from "./ArticuloRecIndiv";
+import axios from "axios";
 
-const ArticulosRec = () => {
-    return (
-        <Col className='ms-5'>
-        <h4 className='fw-bolder'>Otros articulos que te pueden interesar</h4>
-           <ArticuloRecInd></ArticuloRecInd> 
-           <ArticuloRecInd></ArticuloRecInd> 
-           <ArticuloRecInd></ArticuloRecInd> 
-        </Col>
-    );
+const ArticulosRec = ({ categoria }) => {
+  const [articulos, setArticulos] = useState([]);
+  useEffect(() => {
+    const traerArticulos = async () => {
+      try {
+        const respuesta = await axios.get(
+          `http://localhost:4000/articulos/?categoria=${categoria}`
+        );
+        setArticulos(respuesta.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    traerArticulos();
+  }, [categoria]);
+
+  return (
+    <Col className="ms-5">
+      <h4 className="fw-bolder">Otros articulos que te pueden interesar</h4>
+      {articulos.map((articulo) => (
+        <ArticuloRecInd key={articulo.id} articulo={articulo}></ArticuloRecInd>
+      ))}
+    </Col>
+  );
 };
 
 export default ArticulosRec;
