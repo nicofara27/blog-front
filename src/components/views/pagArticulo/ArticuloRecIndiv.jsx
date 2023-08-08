e556eac4a9b9e34c2145a8d97167f76a7e1cb8ed
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-08 12:23:11 -0300

    Cambie el tamaño de la letra del titulo y el boton de los articulos recomendados

--- src/components/views/pagArticulo/ArticuloRecIndiv.jsx
+++ src/components/views/pagArticulo/ArticuloRecIndiv.jsx
@@ -7,9 +7,9 @@ const ArticuloRecInd = ({ articulo }) => {
       <div>
         <img src={articulo.img} alt={articulo.titulo} className="w-100" />
       </div>
-      <h6 className="fw-bolder">{articulo.titulo}</h6>
+      <h6 className="fw-bolder fs-5">{articulo.titulo}</h6>
       <Button
-        className="fs-7 py-0 px-1 w-100"
+        className="py-1 w-100"
         variant="outline-primary"
         href={`/articulo/${articulo.id}`}
       >
