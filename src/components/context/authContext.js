2d182e9a3c557d8369a8031fc76bec1aabb87da7
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-08 11:10:33 -0300

    Di formato al documento

--- src/components/context/authContext.js
+++ src/components/context/authContext.js
@@ -9,13 +9,20 @@ export const AuthContextProvider = ({ children }) => {
   );
 
   const login = async (datos) => {
-    const res = await axios.post("http://localhost:4000/usuarios/login", datos, {withCredentials: true, credentials: 'include'});
+    const res = await axios.post(
+      "http://localhost:4000/usuarios/login",
+      datos,
+      { withCredentials: true, credentials: "include" }
+    );
     setUsuarioActivo(res.data.nombreUsuario);
   };
-  
+
   const logout = async (datos) => {
-    await axios.post("http://localhost:4000/usuarios/logout", {} ,{withCredentials: true, credentials: 'include'});
-    console.log(axios.post("http://localhost:4000/usuarios/logout"))
+    await axios.post(
+      "http://localhost:4000/usuarios/logout",
+      {},
+      { withCredentials: true, credentials: "include" }
+    );
     setUsuarioActivo(null);
   };
 
