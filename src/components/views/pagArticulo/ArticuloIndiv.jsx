7a5ba6960049910ff28e2c369c0778b8e2f1403c
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-08 22:39:42 -0300

    Agregue condicional que comprueba si el usuario activo es el creador del articulo

--- src/components/views/pagArticulo/ArticuloIndiv.jsx
+++ src/components/views/pagArticulo/ArticuloIndiv.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Col, Row } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
 import axios from "axios";
 import moment from "moment/moment";
 import "moment/locale/es";
 import DOMPurify from "dompurify";
 
 const ArticuloInd = ({ articulo }) => {
+  const { usuarioActivo } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const borrarArticulo = async () => {
@@ -20,16 +22,8 @@ const ArticuloInd = ({ articulo }) => {
       console.log(error);
     }
   };
-
-  return (
-    <Col xs={8}>
-      <div id="art__imgContainer">
-        <img
-          src={articulo.img}
-          alt={articulo.titulo}
-          className="art__img w-100 h-100"
-        />
-      </div>
+  const condicionalUsuario =
+    usuarioActivo === articulo.nombreUsuario ? (
       <Row className="w-50 mt-2 align-items-center">
         <Col>
           <img
@@ -55,6 +49,32 @@ const ArticuloInd = ({ articulo }) => {
           </Link>
         </Col>
       </Row>
+    ) : (
+      <Row className="w-50 mt-2 align-items-center">
+        <Col xs={4}>
+          <img
+            src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
+            alt=""
+            className="w-100 h-100"
+          />
+        </Col>
+        <Col xs={6}>
+          <h5 className="m-0">{articulo.nombreUsuario}</h5>
+          <p className="fs-7 m-0">subido {moment(articulo.fecha).fromNow()}</p>
+        </Col>
+      </Row>
+    );
+
+  return (
+    <Col xs={8}>
+      <div id="art__imgContainer">
+        <img
+          src={articulo.img}
+          alt={articulo.titulo}
+          className="art__img w-100 h-100"
+        />
+      </div>
+      {condicionalUsuario}
       <div className="mt-5">
         <h1 className="fw-bolder">{articulo.titulo}</h1>
 
