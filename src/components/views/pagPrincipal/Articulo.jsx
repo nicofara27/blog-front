addd2076189f078ca1a1ee15b5a435fc29684a04
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-08 22:40:33 -0300

    Agregue funcion para mostrar el texto sin las etiquetas y para recortarlo en caso  de ser muy largo

--- src/components/views/pagPrincipal/Articulo.jsx
+++ src/components/views/pagPrincipal/Articulo.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import { Button, Col, Row } from 'react-bootstrap';
+import React from "react";
+import { Button, Col, Row } from "react-bootstrap";
 
-const Articulo = ({articulo}) => {
-    const convertirTexto=(texto)=>{
-        const textoConvertido=new DOMParser().parseFromString(texto,"text/html")
-        return textoConvertido.body.textContent;
-      }
-    
-    return (
-        <Row className='my-5 articulo'>
-            <Col className='d-flex flex-column justify-content-between'>
-            <h1 className='fw-bolder'>{articulo.titulo}</h1>
-            <p>{convertirTexto(articulo.texto)}</p>
-            <Button href={`/articulo/${articulo.id}`} variant="outline-primary">Ver mas</Button>
-            </Col>
-            <Col>
-                <div className='articulo__imgContainer'>
-                    <img className='articulo__img' src=	{articulo.img} alt={articulo.titulo} />
-                </div>
-            </Col>
-        </Row>
-    );
+const Articulo = ({ articulo }) => {
+  const convertirTexto = (texto) => {
+    const textoConvertido = new DOMParser().parseFromString(texto, "text/html");
+    let textoNuevo = textoConvertido.body.textContent;
+    if (textoNuevo.length > 300) {
+      textoNuevo = textoNuevo.slice(0, 300) + "...";
+    }
+    return textoNuevo;
+  };
+
+  return (
+    <Row className="articulo">
+      <Col className="d-flex flex-column justify-content-between">
+        <h2 className="fw-bolder">{articulo.titulo}</h2>
+        <p>{convertirTexto(articulo.texto)}</p>
+        <Button href={`/articulo/${articulo.id}`} variant="outline-primary">
+          Ver mas
+        </Button>
+      </Col>
+      <Col>
+        <div className="articulo__imgContainer">
+          <img
+            className="articulo__img"
+            src={articulo.img}
+            alt={articulo.titulo}
+          />
+        </div>
+      </Col>
+    </Row>
+  );
 };
 
 export default Articulo;
