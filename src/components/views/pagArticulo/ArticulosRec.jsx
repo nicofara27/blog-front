d0b32fa14859e1c35f39c8db3e54a4f1e5f32411
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-10 18:55:47 -0300

    Cambio para que la lista de articulos recomendados no muestre el articulo abierto

--- src/components/views/pagArticulo/ArticulosRec.jsx
+++ src/components/views/pagArticulo/ArticulosRec.jsx
@@ -4,7 +4,7 @@ import ArticuloRecInd from "./ArticuloRecIndiv";
 import axios from "axios";
 const URL = process.env.REACT_APP_API_ARTICULOS;
 
-const ArticulosRec = ({ categoria }) => {
+const ArticulosRec = ({ articulo, categoria }) => {
   const [articulos, setArticulos] = useState([]);
   useEffect(() => {
     const traerArticulos = async () => {
@@ -21,8 +21,10 @@ const ArticulosRec = ({ categoria }) => {
   return (
     <Col className="ms-md-5">
       <h4 className="fw-bolder">Otros articulos que te pueden interesar</h4>
-      {articulos.map((articulo) => (
-        <ArticuloRecInd key={articulo.id} articulo={articulo}></ArticuloRecInd>
+      {articulos.map((articuloRec) => (
+        articuloRec.titulo!==articulo.titulo 
+        ? (<ArticuloRecInd key={articuloRec.id} articulo={articuloRec}></ArticuloRecInd>)
+        : null
       ))}
     </Col>
   );
