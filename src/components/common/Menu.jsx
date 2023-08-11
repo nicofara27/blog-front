8a4b46284739514dc3db6fb1d45b415ef2706160
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-11 17:54:13 -0300

    Cambio para que el boton de subir articulo unicamente aparezca cuando haya un usuario conectado y cambios esteticos en el navbar

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -16,28 +16,54 @@ const Menu = () => {
     <Navbar expand="lg">
       <Container>
         <Navbar.Brand id="logo" href="/">
-          <img className="w-100" src="https://w7.pngwing.com/pngs/254/281/png-transparent-social-media-blogger-computer-icons-logo-blog-text-logo-social-media.png" alt="" />
+          <img
+            className="w-100"
+            src="https://w7.pngwing.com/pngs/254/281/png-transparent-social-media-blogger-computer-icons-logo-blog-text-logo-social-media.png"
+            alt=""
+          />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto fs-5">
-            <Nav.Link className="me-lg-4" href="/?categoria=arte">Arte</Nav.Link>
-            <Nav.Link className="me-lg-4" href="/?categoria=musica">Musica</Nav.Link>
-            <Nav.Link className="me-lg-4" href="/?categoria=cine">Cine</Nav.Link>
-            <Nav.Link className="me-lg-4" href="/?categoria=comida">Comida</Nav.Link>
-            <Nav.Link className="me-lg-4" href="/?categoria=ciencia">Ciencia</Nav.Link>
-            <Nav.Link className="me-lg-4" href="/?categoria=tecnologia">Tecnologia</Nav.Link>
+            <Nav.Link className="me-lg-4" href="/?categoria=arte">
+              Arte
+            </Nav.Link>
+            <Nav.Link className="me-lg-4" href="/?categoria=musica">
+              Musica
+            </Nav.Link>
+            <Nav.Link className="me-lg-4" href="/?categoria=cine">
+              Cine
+            </Nav.Link>
+            <Nav.Link className="me-lg-4" href="/?categoria=comida">
+              Comida
+            </Nav.Link>
+            <Nav.Link className="me-lg-4" href="/?categoria=ciencia">
+              Ciencia
+            </Nav.Link>
+            <Nav.Link className="me-lg-4" href="/?categoria=tecnologia">
+              Tecnologia
+            </Nav.Link>
             {usuarioActivo ? (
-              <div className="d-flex">
-                <Nav.Link className="me-lg-4" disabled>{usuarioActivo}</Nav.Link>
-                <Nav.Link onClick={cerrarSesion}>Logout</Nav.Link>
+              <div className="d-flex flex-column flex-lg-row">
+                <Nav.Link disabled>{usuarioActivo}</Nav.Link>
+                <Nav.Link
+                  className="text-danger me-lg-4"
+                  onClick={cerrarSesion}
+                >
+                  Logout
+                </Nav.Link>
+                <Button className="subir px-2 fs-5" href="/subir">
+                  Subir
+                </Button>
               </div>
             ) : (
-              <Nav.Link className="me-lg-4" href="/ingreso">Login</Nav.Link>
+              <Nav.Link
+                className="me-lg-4 bg-primary rounded-pill px-3 text-light"
+                href="/ingreso"
+              >
+                Login
+              </Nav.Link>
             )}
-            <Button className="subir px-2 fs-5" href="/subir">
-              Subir
-            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
