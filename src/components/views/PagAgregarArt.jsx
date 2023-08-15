0c58eb703bccc885570e70bff8d4c75c8e705338
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-08-15 13:06:47 -0300

    Corregi estilo en responsive

--- src/components/views/PagAgregarArt.jsx
+++ src/components/views/PagAgregarArt.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 import moment from "moment/moment";
 import React, { useState } from "react";
-import { Alert, Button, Col, Form } from "react-bootstrap";
+import { Alert, Button, Col, Container, Form } from "react-bootstrap";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useLocation, useNavigate } from "react-router-dom";
 const URL = process.env.REACT_APP_API_ARTICULOS;
 
 const PagAgregarArt = () => {
+  const art = useLocation().state;
   const [titulo, setTitulo] = useState(art ? art.titulo : "");
   const [texto, setTexto] = useState(art ? art.texto : "");
   const [img, setImg] = useState(art ? art.img : "");
@@ -15,7 +16,6 @@ const PagAgregarArt = () => {
   const [error, setError] = useState(false);
 
   // Contiene los datos de un articulo en el caso de que se lo quiera editar
-  const art = useLocation().state;
 
   const navigate = useNavigate();
 
@@ -66,105 +66,111 @@ const PagAgregarArt = () => {
       ) : (
         []
       )}
-      <Form
-        className="d-flex justify-content-between mx-5"
-        onSubmit={subirArticulo}
-      >
-        <Col xs={8}>
-          <div className="formAgregar__titulo">
-            <Form.Control
-              type="text"
-              value={titulo}
-              onChange={(e) => setTitulo(e.target.value)}
-              placeholder="Titulo"
-            />
-          </div>
-          <div className="formAgregar__quill">
-            <ReactQuill
-              className="formAgregar__quill"
-              theme="snow"
-              value={texto}
-              onChange={setTexto}
-            />
-          </div>
-        </Col>
-        <Col xs={4}>
-          <div className="border p-2 ms-3 h-50">
-            <h4 className="fw-semibold">Publicar</h4>
-            <p className="m-0">
-              <strong>Estatus:</strong> Borrador
-            </p>
-            <p className="mb-2">
-              <strong>Visibilidad:</strong> Publico
-            </p>
-            <Form.Control
-              type="text"
-              placeholder="Imagen"
-              value={img}
-              onChange={(e) => setImg(e.target.value)}
-            />
-            <div className="d-flex justify-content-between mt-3">
-              <Button disabled variant="outline-danger">
-                Guardar como borrador
-              </Button>
-              <Button type="submit" variant="primary">
-                {art ? "Editar" : "Subir"}
-              </Button>
+      <Container>
+        <Form
+          className="d-flex flex-column flex-lg-row justify-content-between"
+          onSubmit={subirArticulo}
+        >
+          <Col xs={12} lg={8} className="mb-2 mb-lg-0">
+            <div className="formAgregar__titulo">
+              <Form.Control
+                type="text"
+                value={titulo}
+                onChange={(e) => setTitulo(e.target.value)}
+                placeholder="Titulo"
+              />
             </div>
-          </div>
-          <div className="border p-2 ms-3 mt-3 h-50">
-            <h4 className="fw-semibold">Categoria</h4>
-            <Form.Check
-              label="Arte"
-              name="categoria"
-              type="radio"
-              value={`arte`}
-              onChange={(e) => setCategoria(e.target.value)}
-              checked={categoria === "arte"}
-            />
-            <Form.Check
-              label="Musica"
-              name="categoria"
-              type="radio"
-              value={`musica`}
-              onChange={(e) => setCategoria(e.target.value)}
-              checked={categoria === "musica"}
-            />
-            <Form.Check
-              label="Cine"
-              name="categoria"
-              type="radio"
-              value={`cine`}
-              onChange={(e) => setCategoria(e.target.value)}
-              checked={categoria === "cine"}
-            />
-            <Form.Check
-              label="Comida"
-              name="categoria"
-              type="radio"
-              value={`comida`}
-              onChange={(e) => setCategoria(e.target.value)}
-              checked={categoria === "comida"}
-            />
-            <Form.Check
-              label="Ciencia"
-              name="categoria"
-              type="radio"
-              value={`ciencia`}
-              onChange={(e) => setCategoria(e.target.value)}
-              checked={categoria === "ciencia"}
-            />
-            <Form.Check
-              label="Tecnologia"
-              name="categoria"
-              type="radio"
-              value={`tecnologia`}
-              onChange={(e) => setCategoria(e.target.value)}
-              checked={categoria === "tecnologia"}
-            />
-          </div>
-        </Col>
-      </Form>
+            <div className="formAgregar__quill">
+              <ReactQuill
+                className=""
+                theme="snow"
+                value={texto}
+                onChange={setTexto}
+              />
+            </div>
+          </Col>
+          <Col
+            xs={12}
+            lg={4}
+            className="d-flex flex-column justify-content-center"
+          >
+            <div className="border p-2 ms-lg-3 mt-3 h-50 order-1">
+              <h4 className="fw-semibold">Publicar</h4>
+              <p className="m-0">
+                <strong>Estatus:</strong> Borrador
+              </p>
+              <p className="mb-2">
+                <strong>Visibilidad:</strong> Publico
+              </p>
+              <Form.Control
+                type="text"
+                placeholder="Imagen"
+                value={img}
+                onChange={(e) => setImg(e.target.value)}
+              />
+              <div className="d-flex justify-content-between mt-3 order-0">
+                <Button disabled variant="outline-danger">
+                  Guardar como borrador
+                </Button>
+                <Button type="submit" variant="primary">
+                  {art ? "Editar" : "Subir"}
+                </Button>
+              </div>
+            </div>
+            <div className="border p-2 ms-lg-3 mt-3 mt-lg-0 h-50">
+              <h4 className="fw-semibold">Categoria</h4>
+              <Form.Check
+                label="Arte"
+                name="categoria"
+                type="radio"
+                value={`arte`}
+                onChange={(e) => setCategoria(e.target.value)}
+                checked={categoria === "arte"}
+              />
+              <Form.Check
+                label="Musica"
+                name="categoria"
+                type="radio"
+                value={`musica`}
+                onChange={(e) => setCategoria(e.target.value)}
+                checked={categoria === "musica"}
+              />
+              <Form.Check
+                label="Cine"
+                name="categoria"
+                type="radio"
+                value={`cine`}
+                onChange={(e) => setCategoria(e.target.value)}
+                checked={categoria === "cine"}
+              />
+              <Form.Check
+                label="Comida"
+                name="categoria"
+                type="radio"
+                value={`comida`}
+                onChange={(e) => setCategoria(e.target.value)}
+                checked={categoria === "comida"}
+              />
+              <Form.Check
+                label="Ciencia"
+                name="categoria"
+                type="radio"
+                value={`ciencia`}
+                onChange={(e) => setCategoria(e.target.value)}
+                checked={categoria === "ciencia"}
+              />
+              <Form.Check
+                label="Tecnologia"
+                name="categoria"
+                type="radio"
+                value={`tecnologia`}
+                onChange={(e) => setCategoria(e.target.value)}
+                checked={categoria === "tecnologia"}
+              />
+            </div>
+          </Col>
+        </Form>
+      </Container>
     </main>
   );
 };
