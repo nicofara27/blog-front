60d297f2188c2939e9b07499b1e2b3d102bf1040
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-02 18:58:11 -0300

    Pase el condicional y la funcion que comprueba si hay articulos a un componentes individual

--- src/components/views/PagPrincipal.jsx
+++ src/components/views/PagPrincipal.jsx
@@ -1,44 +1,16 @@
-import React, { useEffect, useState } from "react";
-import Articulo from "./pagPrincipal/Articulo";
-import { Container } from "react-bootstrap";
-import axios from "axios";
-import { useLocation } from "react-router-dom";
-const URL = process.env.REACT_APP_API_ARTICULOS;
+import { useEffect, useState } from "react";
+import CondicionalArticulos from "./pagPrincipal/CondicionalArticulos";
 
 const PagPrincipal = () => {
-  const [articulos, setArticulos] = useState([]);
-
-  const categoria = useLocation().search;
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Funcion para traer los articulos
-    const traerArticulos = async () => {
-      try {
-        const respuesta = await axios.get(URL + `${categoria}`);
-        setArticulos(respuesta.data.reverse());
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    traerArticulos();
-  }, [categoria]);
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
 
   return (
-    <main className="asd">
-      <Container>
-        {categoria ? (
-          <h1 className="pagPrincipal__titulo">{categoria.slice(11)}</h1>
-        ) : null}
-        {articulos.length > 0 ? (
-          articulos.map((articulo) => (
-            <Articulo key={articulo.id} articulo={articulo}></Articulo>
-          ))
-        ) : (
-          <h1 className="pagPrincipal__texto">
-            Por el momento no hay articulos que pertenezcan a esta categoria
-          </h1>
-        )}
-      </Container>
+    <main className="pagPrincipal">
+      <CondicionalArticulos />
     </main>
   );
 };
