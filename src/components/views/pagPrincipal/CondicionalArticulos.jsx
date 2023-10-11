0c887d18c9e8b19bcb9afdf6cc7fc27a9085311d
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-11 10:22:59 -0300

    Corregi condicional que muestra el cartel de cargando o los articulos

--- src/components/views/pagPrincipal/CondicionalArticulos.jsx
+++ src/components/views/pagPrincipal/CondicionalArticulos.jsx
@@ -6,30 +6,29 @@ import { useLocation } from "react-router";
 const URL = process.env.REACT_APP_API_ARTICULOS;
 
 const CondicionalArticulos = () => {
-  const [articulos, setArticulos] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [articulos, setArticulos] = useState(null);
 
   const categoria = useLocation().search;
 
-  const condicionalCargando = loading ? (
-    <Container>
-      <div className="pagPrincipal__cargando">
-        <h1>Cargando Articulos</h1>
-        <Spinner />
-      </div>
-    </Container>
-  ) : articulos.length > 0 ? (
-    articulos.map((articulo) => (
-      <Articulo key={articulo.id} articulo={articulo}></Articulo>
-    ))
-  ) : (
-    <h1 className="pagPrincipal__texto">
-      Por el momento no hay articulos que pertenezcan a esta categoria
-    </h1>
-  );
+  const condicionalCargando =
+    articulos === null ? (
+      <Container>
+        <div className="pagPrincipal__cargando">
+          <h1>Cargando Articulos</h1>
+          <Spinner />
+        </div>
+      </Container>
+    ) : articulos.length > 0 ? (
+      articulos.map((articulo) => (
+        <Articulo key={articulo.id} articulo={articulo}></Articulo>
+      ))
+    ) : (
+      <h1 className="pagPrincipal__texto">
+        Por el momento no hay articulos que pertenezcan a esta categoria
+      </h1>
+    );
 
   useEffect(() => {
-    // Funcion para traer los articulos
     const traerArticulos = async () => {
       try {
         const respuesta = await axios.get(URL + `${categoria}`);
@@ -40,15 +39,10 @@ const CondicionalArticulos = () => {
     };
     traerArticulos();
   }, [categoria]);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
 
   return (
     <Container className="algin-self-start">
-      {categoria && !loading ? (
+      {categoria ? (
         <h1 className="pagPrincipal__titulo">{categoria.slice(11)}</h1>
       ) : null}
       {condicionalCargando}
