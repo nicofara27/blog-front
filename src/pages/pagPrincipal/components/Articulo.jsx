a3c9e9a47de1954bf91353eeb0c0cd1c5a46766c
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-11 19:08:33 -0300

    Cambie de posicion una funcion

--- src/pages/pagPrincipal/components/Articulo.jsx
+++ src/pages/pagPrincipal/components/Articulo.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Button, Col, Row } from "react-bootstrap";
 
-const Articulo = ({ articulo }) => {
-  // Funcion que convierte el texto que se sube con react-quill con etiquetas al formato HTMLDocument
-  const convertirTexto = (texto) => {
-    const textoConvertido = new DOMParser().parseFromString(texto, "text/html");
-    let textoNuevo = textoConvertido.body.textContent;
-    // En el caso de que el texto sea muy largo lo recorta
-    if (textoNuevo.length > 300) {
-      textoNuevo = textoNuevo.slice(0, 300) + "...";
-    }
-    return textoNuevo;
-  };
+// Funcion que convierte el texto que se sube con react-quill con etiquetas al formato HTMLDocument
+const convertirTexto = (texto) => {
+  const textoConvertido = new DOMParser().parseFromString(texto, "text/html");
+  let textoNuevo = textoConvertido.body.textContent;
+  // En el caso de que el texto sea muy largo lo recorta
+  if (textoNuevo.length > 300) {
+    textoNuevo = textoNuevo.slice(0, 300) + "...";
+  }
+  return textoNuevo;
+};
 
+const Articulo = ({ articulo }) => {
   return (
     <Row className="articulo mb-5">
       <Col
