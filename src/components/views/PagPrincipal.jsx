915e104adc155066c08801960ad548a1cab3849e
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-11 10:23:49 -0300

    Elimine parte del condicional que ya usaba

--- src/components/views/PagPrincipal.jsx
+++ src/components/views/PagPrincipal.jsx
@@ -1,13 +1,6 @@
-import { useEffect, useState } from "react";
 import CondicionalArticulos from "./pagPrincipal/CondicionalArticulos";
 
 const PagPrincipal = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 3000);
-  }, []);
-
   return (
     <main className="pagPrincipal">
       <CondicionalArticulos />
